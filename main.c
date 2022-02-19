1895a7a89f689baa654aa4e89f4298076ce96c35
marshmellow0007 <97960418+marshmellow0007@users.noreply.github.com>
2022-02-19 13:18:29 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+  county library systems
+  marshmellow
+  Jan,2021
+  Compiler: GNU GNC
+  Language C89
+  License: MIT
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("County library. \n");
+    printf("Welcome Marshmellow. \n");
+    printf("What would you like to do?\n");
+    printf("1. Add new User \n");
+    printf("2. Add Book. \n");
+    printf("3. Add row \n");
+    printf("4. Add shelf number \n");
+
+    return 0;
+}
