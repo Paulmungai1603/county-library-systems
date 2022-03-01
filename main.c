1c13e4991cb4e2667c431ae928bfc1c6b6a2667e
marshmellow0007 <97960418+marshmellow0007@users.noreply.github.com>
2022-03-01 17:18:51 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -10,15 +10,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("County library. \n");
-    printf("Welcome Marshmellow. \n");
-    printf("What would you like to do?\n");
-    printf("1. Add new User \n");
-    printf("2. Add Book. \n");
-    printf("3. Add row \n");
-    printf("4. Add shelf number \n");
+int main() {
+    int action;
+    printf("select an action below\n");
+    printf("1. Add new patron\n");
+    printf("2. view all patrons\n");
+    printf("3. View all books\n");
+    printf("4. Add new Book\n");
+    printf("Your action: \n");
+    if(action< 1|| action>4)
+      printf("invalid action. Try again\n");
+
+      return 0;
+ void execute_action(int action){
+    switch(action){
+    case 1:
+        printf("adding a new patron\n");
+        break;
+    case 2:
+        printf("list of all patrons\n");
+        break;
+    case 3:
+        printf("list os all books\n");
+        break;
+    case 4:
+        printf("adding a new book\n");
+        break;
+    default: printf(" invalid action.\n");
+    }
+ }
 
     return 0;
 }
